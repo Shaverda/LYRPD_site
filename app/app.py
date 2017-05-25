4412c20c570b9c4973769186d4ccc1915b8af4e3
Walter Moreira <walter@waltermoreira.net>
2017-05-24 22:35:46 -0500

    Update images if necessary

--- app/app.py
+++ app/app.py
@@ -9,9 +9,8 @@ import httplib2
 from flask import Flask, render_template, request, redirect, flash
 from flask import send_from_directory
 import pygsheets
-from oauth2client.service_account import ServiceAccountCredentials
-from googleapiclient.http import MediaIoBaseDownload
-from apiclient import discovery
+
+import get_images
 
 
 def get_env_vars(*names):
@@ -26,8 +25,8 @@ def get_env_vars(*names):
         sys.exit(1)
 
 
-MAPS_KEY, SHEET, FOLDER, GOOGLE_CREDENTIALS = get_env_vars(
-    'MAPS_KEY', 'SHEET', 'FOLDER', 'GOOGLE_CREDENTIALS')
+MAPS_KEY, SHEET, GOOGLE_CREDENTIALS = get_env_vars(
+    'MAPS_KEY', 'SHEET', 'GOOGLE_CREDENTIALS')
 
 
 app = Flask(__name__)
@@ -38,38 +37,9 @@ def get_sheet(sheet_id):
     sheet = c.open_by_key(sheet_id)
     return sheet.worksheet_by_title('Sheet1')
 
-def get_drive():
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        GOOGLE_CREDENTIALS, scopes=['https://www.googleapis.com/auth/drive'])
-    http = creds.authorize(httplib2.Http())
-    return discovery.build('drive', 'v3', http=http)
-
-
-def get_picture(drive, folder, name):
-    q = "'{folder}' in parents and not trashed".format(folder=folder)
-    files = drive.files().list(
-        q=q, fields='files(description,id,name)').execute()
-    for file in files['files']:
-        name, _ = os.path.splitext(file['name'])
-        if name == name:
-            target = os.path.join('static/images', file['name'])
-            download(drive, file['id'], target)
-            return target
-    raise Exception(name)
-
-
-def download(drive, file_id, target):
-    request = drive.files().get_media(fileId=file_id)
-    fh = io.BytesIO()
-    downloader = MediaIoBaseDownload(fh, request)
-    done = False
-    while done is False:
-        status, done = downloader.next_chunk()
-    with open(target, 'w+b') as out:
-        out.write(fh.getvalue())
-
         
 sheet = get_sheet(SHEET)
+last_update = None
 
 
 def get_data(sheet):
@@ -79,15 +49,23 @@ def get_data(sheet):
                record['Website'], record['Activity'], record['image name']]
         
 
+def update():
+    global last_update
+    
+    data = list(get_data(sheet))
+    d = get_images.download_updated(last_update)
+    if d:
+        print('Refreshed images')
+        last_update = d
+    return data
+        
+
 @app.route('/')
 def main():
-    data = list(get_data(sheet))
+    data = update()
     return render_template(
         'index.html',
         maps_key=MAPS_KEY,
         data=json.dumps(data))
-    #     sheet_link='https://docs.google.com/spreadsheets/d/{}'.format(SHEET),
-    #     folder_link='https://drive.google.com/drive/u/1/folders/{}'.format(FOLDER)
-    # )
 
 
---
+++ app/get_images.py
@@ -0,0 +1,86 @@
+import datetime
+import httplib2
+import io
+import os
+
+from oauth2client.service_account import ServiceAccountCredentials
+from googleapiclient.http import MediaIoBaseDownload
+from apiclient import discovery
+
+def get_env_vars(*names):
+    missing = []
+    for name in names:
+        try:
+            yield os.environ[name]
+        except KeyError:
+            missing.append(name.upper())
+    if missing:
+        print('Environment variables {0} are needed'.format(', '.join(missing)))
+        sys.exit(1)
+
+
+FOLDER, GOOGLE_CREDENTIALS = get_env_vars(
+    'FOLDER', 'GOOGLE_CREDENTIALS')
+
+
+def get_drive():
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        GOOGLE_CREDENTIALS, scopes=['https://www.googleapis.com/auth/drive'])
+    http = creds.authorize(httplib2.Http())
+    return discovery.build('drive', 'v3', http=http)
+
+
+def get_picture(drive, folder, name):
+    q = "'{folder}' in parents and not trashed".format(folder=folder)
+    files = drive.files().list(
+        q=q, fields='files(description,id,name)').execute()
+    for file in files['files']:
+        name, _ = os.path.splitext(file['name'])
+        if name == name:
+            target = os.path.join('static/images', file['name'])
+            download(drive, file['id'], target)
+            return target
+    raise Exception(name)
+
+
+def list_updated(drive, folder, since=None):
+    if since is None:
+        since = datetime.datetime.fromtimestamp(0)
+    q = "'{FOLDER}' in parents and modifiedTime > '{SINCE}'".format(
+        FOLDER=folder,
+        SINCE=since.isoformat())
+    files = drive.files().list(
+        q=q, fields='files(description,id,name)').execute()
+    if not files['files']:
+        return
+
+    q = "'{FOLDER}' in parents and not trashed".format(FOLDER=folder)
+    files = drive.files().list(
+        q=q, fields='files(description,id,name)').execute()
+
+    for file in files['files']:
+        yield file
+    
+
+def download(drive, file_id, target):
+    request = drive.files().get_media(fileId=file_id)
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+    with open(target, 'w+b') as out:
+        out.write(fh.getvalue())
+
+
+def download_new(drive, folder, last_update=None):
+    updated = list(list_updated(drive, folder, last_update))
+    for file in updated:
+        download(drive, file['id'], os.path.join('static/images', file['name']))
+    if updated:
+        return datetime.datetime.utcnow()
+
+
+def download_updated(last_update=None):
+    drive = get_drive()
+    return download_new(drive, FOLDER, last_update)
